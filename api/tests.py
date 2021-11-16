05c566eb918e9117419e0c48c16d79e95b365223
Nour94276 <77611129+Nour94276@users.noreply.github.com>
2021-11-16 13:26:47 +0100

    test unitaire pour MAJ SUPP un utilisateur et ajouter un animé dans sa watchlist

--- api/tests.py
+++ api/tests.py
@@ -26,5 +26,44 @@ class ViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)  # tout va bien
         # verifier le nom d"utilisateur qui s'est connecté
         self.assertEqual(response.data['user']['nom'], "uchiha")
+ 
+    def test_update_user(self):
+        """Met à jour l'utilisateur"""
 
-        
+        response = self.client.put(
+            'http://localhost:8000/api/user/' + str(self.id_user) + '/', {
+                "nom": "God",
+                "prenom": "Madara",
+                "mdp": "hashirama123",
+                "mail": "susano@gmail.com"
+            }, content_type="application/json")
+
+        self.assertEqual(response.status_code, 200)  # tout va bien
+
+        response = self.client.get(
+            'http://localhost:8000/api/user/' + str(self.id_user) + '/')
+
+        # verifier si la mise à jour a bien été effectué
+        self.assertEqual(response.data['nom'], "God")
+        self.assertEqual(response.data['prenom'], "Madara")
+
+    def test_delete_user(self):
+        """Supprimer un utilisateur"""
+
+        response = self.client.delete(
+            'http://localhost:8000/api/user/' + str(self.id_user) + '/')
+
+        self.assertEqual(response.status_code, 204)  # tout va bien
+
+        response = self.client.get(
+            'http://localhost:8000/api/user/' + str(self.id_user) + '/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_add_to_watchlist(self):
+        """Ajouter un Anime à sa WatchList"""
+
+        response = self.client.post(
+            'http://127.0.0.1:8000/api/watchlist/', {"userId": self.id_user, "tmdbId": 6949, "name": "Tokyo Revengers", "imageUrl": "https://tokyo.png"})
+
+        # tout va bien, l'anime de l'utilisateur a été ajouté dans sa watchlist
+        self.assertEqual(response.status_code, 201)
